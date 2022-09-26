b2b7351eb2d62bb9c4b0aaa4306333c1ac20b7ef
manpreet <manpreetg@mangospring.com>
2022-09-26 12:17:20 +0530

    DragIT project upgrade changes

--- app/src/main/java/com/example/manpreet/dragit/LoginActivity.java
+++ app/src/main/java/com/example/manpreet/dragit/LoginActivity.java
@@ -5,13 +5,15 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Environment;
-import android.support.design.widget.TextInputLayout;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.io.File;
 import java.io.FileNotFoundException;
